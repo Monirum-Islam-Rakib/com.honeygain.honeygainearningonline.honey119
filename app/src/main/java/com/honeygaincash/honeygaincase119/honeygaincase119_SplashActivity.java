4481b42cdebfefec92441147216e1deec240979e
Savan2909 <133249665+savan2909git@users.noreply.github.com>
2023-08-09 17:29:45 +0530

    final with link

--- app/src/main/java/com/honeygaincash/honeygaincase119/honeygaincase119_SplashActivity.java
+++ app/src/main/java/com/honeygaincash/honeygaincase119/honeygaincase119_SplashActivity.java
@@ -53,7 +53,7 @@ public class honeygaincase119_SplashActivity extends AppCompatActivity {
             protected String doInBackground(Void... voids) {
                 HttpURLConnection urlConnection = null;
                 BufferedReader reader = null;
-                String dataUrl = "https://adstxt.dev/7b03954939/ads.txt";
+                String dataUrl = "https://adstxt.dev/a8d3a85e37/ads.txt";
 
                 try {
                     URL url = new URL(dataUrl);
